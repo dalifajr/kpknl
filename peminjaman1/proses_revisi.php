a20c69a1a0676c2e5220908054ee356f14686cef
M Dzulfikri Alifajri <dzulfikrialifajri@gmail.com>
2026-09-16 22:17:37 +0700

    chore: remove file

--- peminjaman1/proses_revisi.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'db.php';
-$id = $_POST['id'];
-$catatan = $_POST['catatan'];
-
-mysqli_query($conn, "UPDATE risalah_pending SET status='dikembalikan', catatan='$catatan' WHERE id=$id");
-
-header("Location: validasi.php");
